bf6a11007dea0659e4556f7f6716de379349c044
Plumtree3D <ham.in.kneesocks@gmail.com>
2023-02-02 12:22:05 +0100

    feat: added draggable button that logs it's position

--- web-app/src/pages/Home/Home.styled.ts
+++ web-app/src/pages/Home/Home.styled.ts
@@ -1,10 +1,14 @@
 import styled from "styled-components";
-import { MapContainer } from 'react-leaflet'
+import { MapContainer, Popup } from 'react-leaflet'
 
 export const LeafletContainer = styled(MapContainer)`
   width: 100vw;
   height: 100vh;
 `;
+
+export const PinPopup = styled(Popup)`
+  position: fixed;
+`
 
 export const CardRow = styled.section`
   display: flex;
@@ -12,3 +16,20 @@ export const CardRow = styled.section`
   gap: 20px;
   justify-content: center;
 `;
+
+export const Container = styled.section`
+  position: relative;
+`
+
+export const CreateNewPin = styled.button`
+  margin: 10px;
+  padding: 6px;
+  border-radius: 15px;
+  background-color: #93CFD2;
+  box-shadow: 3px 3px 0 #31777A;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 1000;
+`;
+
--- web-app/src/pages/Home/Home.tsx
+++ web-app/src/pages/Home/Home.tsx
@@ -1,16 +1,26 @@
+import { useEffect, useState, useMemo, useRef } from 'react';
 import { 
   Marker, 
   Popup, 
   TileLayer, 
-  useMap
+  useMap,
+  Pane,
+  Tooltip
 } from 'react-leaflet';
 import L from 'leaflet';
-import { LeafletContainer } from "./Home.styled";
+import { 
+  LeafletContainer, 
+  PinPopup, 
+  CreateNewPin,
+  Container
+} from "./Home.styled";
 import Loader from "../../components/Loader";
 import { useQuery, gql } from "@apollo/client";
 import { GetPinsQuery } from "../../gql/graphql";
 import markerIcon from "../../media/markers/marker.png"
 import shadowIcon from "../../media/markers/shadow.png"
+import draggableIcon from "../../media/markers/draggable.png"
+
 import "./TooltipStyle.css"
 import { FaHeart } from 'react-icons/fa';
 
@@ -36,17 +46,73 @@ const GET_PINS = gql`
   }
 `;
 
+const outer = [
+  [50.505, -29.09],
+  [52.505, 29.09],
+]
+const inner = [
+  [49.505, -2.09],
+  [53.505, 2.09],
+]
+
 
 const PinMarker  = new L.Icon({
   iconUrl: markerIcon,
   shadowUrl: shadowIcon,
 
-  iconSize:     [51, 68], // size of the icon
-  shadowSize:   [48, 64], // size of the shadow
+  iconSize:     [42, 57], // size of the icon
+  shadowSize:   [38, 55], // size of the shadow
   iconAnchor:   [22, 74], // point of the icon which will correspond to marker's location
   shadowAnchor: [0, 82],  // the same for the shadow
   popupAnchor:  [6, -76] // point from which the popup should open relative to the iconAnchor
 })
+
+const DragMarker  = new L.Icon({
+  iconUrl: draggableIcon,
+  shadowUrl: shadowIcon,
+
+  iconSize:     [42, 57], // size of the icon
+  shadowSize:   [38, 55], // size of the shadow
+  iconAnchor:   [22, 74], // point of the icon which will correspond to marker's location
+  shadowAnchor: [0, 82],  // the same for the shadow
+  popupAnchor:  [6, -76] // point from which the popup should open relative to the iconAnchor
+})
+
+const Location = () => {
+  const map = useMap();
+  const markerRef = useRef(null)
+  const [position, setPosition] = useState<any | null>(null)
+
+  const eventHandlers = useMemo(
+    () => ({
+      dragend(e: any) {
+        console.log(e.target.getLatLng()); 
+      },
+    }),
+    [],
+  )
+
+  useEffect(() => {
+    map.locate({
+      setView: true
+    })
+    map.on('locationfound', (event) => {
+      setPosition(event.latlng)
+    })
+  }, [map])
+
+  return position
+  ? (
+    <>
+      <Marker icon={ DragMarker } 
+      draggable={true} 
+      eventHandlers={eventHandlers} 
+      position={position}
+      ref={markerRef} />
+    </>
+  )
+  : null
+}
 
 type PropType = { id: string; name: string; latitude: number; longitude: number; address: string; description: string;}
 
@@ -56,6 +122,9 @@ const Pin = ({ id, name, latitude, longitude, address, description }: PropType) 
     position={[latitude, longitude]}
     icon={ PinMarker }
     >
+      <Tooltip>
+        {name}
+      </Tooltip>
       <Popup>
         <header className='row title'>
           <span>{name}</span>
@@ -69,8 +138,26 @@ const Pin = ({ id, name, latitude, longitude, address, description }: PropType) 
         </footer>
       </Popup>
     </Marker>
-  );
-};
+  )
+}
+
+// const PopUp = (name, description, adress) => {
+//   return (
+//     <div>
+//       <header className='row title'>
+//         <span>{name}</span>
+//       </header>
+//       <div className='row'>
+//         <p>{description}</p>
+//         <button className='favBtn'><FaHeart/></button>
+//       </div>
+//       <footer>
+//         <p className='adress'>{address}</p>
+//       </footer>
+//     </div>
+//   )
+// }
+
 
 const Home = () => {
   const { data, loading, error, refetch } = useQuery<GetPinsQuery>(
@@ -91,9 +178,10 @@ const Home = () => {
     return (
       <LeafletContainer center={[45.75, 4.85]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />      
+        /> 
+        <Pane name='fixed' style={{ zIndex: 499 }} />
+        <Location/>
         {data.pins.map((pin) => (
         <Pin
           key={pin.id}
@@ -109,9 +197,10 @@ const Home = () => {
     );
   };
   return (
-    <>
+    <Container>
       {renderMainContent()}
-    </>
+      <CreateNewPin> Ajouter un pin </CreateNewPin>
+    </Container>
   );
 };
 
