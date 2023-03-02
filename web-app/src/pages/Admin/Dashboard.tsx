e2637231cd73ea0272f9e540f24c15168111828e
Plumtree3D <ham.in.kneesocks@gmail.com>
2023-03-02 15:36:43 +0100

    fix: categories route

--- web-app/src/pages/Admin/Dashboard.tsx
+++ web-app/src/pages/Admin/Dashboard.tsx
@@ -6,7 +6,7 @@ import {
 import {
   ADMIN_ALL_PINS_PATH,
   MAP_PATH,
-
+  ADMIN_CATEGORIES
 } from "../paths";
 import { DashboardButton } from "../../styles/base-styles"
 import { Grid, Content } from "./Dashboard.styled"
@@ -16,29 +16,28 @@ import { FaUser, FaMapMarked, FaMapMarkerAlt, FaClipboardList } from 'react-icon
 const Dashboard = () => {
   return (
     <>
-    <NavbarPage />
-    <Flex width="full" align="center" justifyContent="center">
-      <Box
-        bg="#fff"
-        p={8}
-        width="1000px"
-        borderWidth={1}
-        borderRadius={8}
-        boxShadow="lg"
-      >
-        <Box textAlign="center">
-          <Heading>Tableau de bord</Heading>
+      <NavbarPage />
+      <Flex width="full" align="center" justifyContent="center">
+        <Box
+          bg="#fff"
+          p={8}
+          width="1000px"
+          borderWidth={1}
+          borderRadius={8}
+          boxShadow="lg"
+        >
+          <Box textAlign="center">
+            <Heading>Tableau de bord</Heading>
+          </Box>
+          <Grid>
+            <DashboardButton to={MAP_PATH}> <Content> <FaMapMarked/> <p>Afficher la carte</p> </Content> </DashboardButton>
+            <DashboardButton to={ADMIN_ALL_PINS_PATH}> <Content> <FaMapMarkerAlt/> <p>Gestion des Pin</p> </Content> </DashboardButton>
+            <DashboardButton to={ADMIN_CATEGORIES}> <Content> <FaClipboardList/> <p>Gestion des Catégories</p> </Content> </DashboardButton>
+            <DashboardButton to=""> <Content> <FaUser/> <p>Gestion des Pinners</p> </Content> </DashboardButton>
+          </Grid>
         </Box>
-        <Grid>
-          <DashboardButton to={MAP_PATH}> <Content> <FaMapMarked/> <p>Afficher la carte</p> </Content> </DashboardButton>
-          <DashboardButton to={ADMIN_ALL_PINS_PATH}> <Content> <FaMapMarkerAlt/> <p>Gestion des Pin</p> </Content> </DashboardButton>
-          <DashboardButton to=""> <Content> <FaClipboardList/> <p>Gestion des Catégories</p> </Content> </DashboardButton>
-          <DashboardButton to=""> <Content> <FaUser/> <p>Gestion des Pinners</p> </Content> </DashboardButton>
-        </Grid>
-
-        </Box>
-        </Flex>
-      </>
+      </Flex>
+    </>
   )
 };
 
