98a835970859670c77b26dec1926f7a4b90513bb
Tritchaphorn BOISSON <tritchaphorn@gmail.com>
2023-07-15 09:10:48 +0200

    remove error in console, admin and protected route: ok

--- web-app/src/pages/Protected/ProtectedRoute.tsx
+++ web-app/src/pages/Protected/ProtectedRoute.tsx
@@ -1,46 +1,24 @@
 import { Box, Flex, Spinner, useToast } from "@chakra-ui/react";
 import { AppContext } from "context/AppContext";
 import { SIGN_IN_PATH } from "pages/paths";
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }: any) => {
-  const toast = useToast();
   const appContext = useContext(AppContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!appContext?.isLoggedIn) {
+      navigate("/sign-in");
+    }
+  }, [appContext, navigate]);
+
   if (appContext?.loading) {
-    return (
-      <>
-        <Flex width="full" align="center" justifyContent="center">
-          <Box
-            bg="#fff"
-            p={8}
-            width="1000px"
-            borderWidth={1}
-            borderRadius={8}
-            boxShadow="lg"
-          >
-            <Spinner
-              thickness="4px"
-              speed="0.65s"
-              emptyColor="gray.200"
-              color="#FF8787"
-              size="xl"
-            />
-          </Box>
-        </Flex>
-      </>
-    );
+    return <div>Loading...</div>;
   }
-  if (!appContext?.isLoggedIn) {
-    toast({
-      title: "Se connecter pour accéder cette page.",
-      status: "info",
-      duration: 5000,
-      isClosable: true,
-    });
-    return <Navigate to={SIGN_IN_PATH} replace />;
-  }
-  return children;
+
+  return appContext?.isLoggedIn ? children : null;
 };
 
 export default ProtectedRoute;
