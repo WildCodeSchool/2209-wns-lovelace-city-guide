04dffc663db28eecae858e3704544bbad6c8b097
Tritchaphorn BOISSON <tritchaphorn@gmail.com>
2023-07-16 08:43:01 +0200

    update protected route

--- web-app/src/pages/Protected/ProtectedRoute.tsx
+++ web-app/src/pages/Protected/ProtectedRoute.tsx
@@ -1,6 +1,4 @@
-import { Box, Flex, Spinner, useToast } from "@chakra-ui/react";
 import { AppContext } from "context/AppContext";
-import { SIGN_IN_PATH } from "pages/paths";
 import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
