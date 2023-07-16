56b05f329e838d94f11a9d7bc068e406468e9939
tritchaphornBOISSON <69003675+tritchaphornBOISSON@users.noreply.github.com>
2023-07-16 14:36:04 +0200

    67 fix double toast on protected routes (#95)
    * remove error in console, admin and protected route: ok
    
    * update protected route

--- web-app/src/pages/Protected/AlreadyLoggedIn.tsx
+++ web-app/src/pages/Protected/AlreadyLoggedIn.tsx
@@ -1,21 +1,51 @@
-import { useToast } from "@chakra-ui/react";
+import { useToast, Flex, Box, Spinner } from "@chakra-ui/react";
 import { AppContext } from "context/AppContext";
 import { HOME_PATH } from "pages/paths";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
 const AlreadyLoggedIn = ({ children }: any) => {
   const toast = useToast();
   const appContext = useContext(AppContext);
+
+  useEffect(() => {
+    if (appContext?.isLoggedIn) {
+      toast({
+        title: "Vous vous êtes déjà connecté.",
+        status: "info",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  }, [appContext?.isLoggedIn, toast]);
+
+  if (appContext?.loading) {
+    return (
+      <Flex width="full" align="center" justifyContent="center">
+        <Box
+          bg="#fff"
+          p={8}
+          width="1000px"
+          borderWidth={1}
+          borderRadius={8}
+          boxShadow="lg"
+        >
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="#FF8787"
+            size="xl"
+          />
+        </Box>
+      </Flex>
+    );
+  }
+
   if (appContext?.isLoggedIn) {
-    toast({
-      title: "Vous vous êtes déjà connecté.",
-      status: "info",
-      duration: 5000,
-      isClosable: true,
-    });
     return <Navigate to={HOME_PATH} replace />;
   }
+
   return children;
 };
 
